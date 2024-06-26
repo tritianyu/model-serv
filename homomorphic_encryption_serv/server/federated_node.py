6a71fad0e1077a42014439c62caadb04d58dbd89
cuitianyu <1468424792@qq.com>
2024-06-26 17:25:28 +0800

    按照平台提供的请求body修改了代码，将client的实例化改为按照modelCalUrlList中的元素个数循环生成，weigh_accumulator不区分client直接进行聚合

--- homomorphic_encryption_serv/server/federated_node.py
+++ homomorphic_encryption_serv/server/federated_node.py
@@ -18,7 +18,7 @@ class Server(object):
 
         self.conf = conf
 
-        self.global_model = models.LR_Model(public_key=Server.public_key, w_size=self.conf["feature_num"] + 1)
+        self.global_model = models.LR_Model(public_key=Server.public_key, w_size=self.conf["modelParams"]["modelData"]["feature_num"] + 1)
 
         self.eval_x = eval_dataset[0]
 
@@ -27,7 +27,7 @@ class Server(object):
     def model_aggregate(self, weight_accumulator):
 
         for id, data in enumerate(self.global_model.encrypt_weights):
-            update_per_layer = weight_accumulator[id] * self.conf["lambda"]
+            update_per_layer = weight_accumulator[id] * self.conf["modelParams"]["modelData"]["lambda"]
 
             self.global_model.encrypt_weights[id] = self.global_model.encrypt_weights[id] + update_per_layer
 
@@ -37,7 +37,7 @@ class Server(object):
         correct = 0
         dataset_size = 0
 
-        batch_num = int(self.eval_x.shape[0] / self.conf["batch_size"])
+        batch_num = int(self.eval_x.shape[0] / self.conf["modelParams"]["modelData"]["batch_size"])
         # print("本轮加密的全局模型参数：")
         # print(self.global_model.encrypt_weights[0].ciphertext())
 
@@ -46,9 +46,9 @@ class Server(object):
         print(self.global_model.weights)
 
         for batch_id in range(batch_num):
-            x = self.eval_x[batch_id * self.conf["batch_size"]: (batch_id + 1) * self.conf["batch_size"]]
+            x = self.eval_x[batch_id * self.conf["modelParams"]["modelData"]["batch_size"]: (batch_id + 1) * self.conf["modelParams"]["modelData"]["batch_size"]]
             x = np.concatenate((x, np.ones((x.shape[0], 1))), axis=1)
-            y = self.eval_y[batch_id * self.conf["batch_size"]: (batch_id + 1) * self.conf["batch_size"]].reshape(
+            y = self.eval_y[batch_id * self.conf["modelParams"]["modelData"]["batch_size"]: (batch_id + 1) * self.conf["modelParams"]["modelData"]["batch_size"]].reshape(
                 (-1, 1))
 
             dataset_size += x.shape[0]
@@ -101,13 +101,13 @@ class Client(object):
 
         neg_one = self.public_key.encrypt(-1)
 
-        for e in range(self.conf["local_epochs"]):
+        for e in range(self.conf["modelParams"]["modelData"]["local_epochs"]):
             print("正在进行本地训练的轮数：", e + 1)
             if e > 0 and e % 2 == 0:
                 self.local_model.encrypt_weights = Server.re_encrypt(self.local_model.encrypt_weights)
 
             idx = np.arange(self.data_x.shape[0])
-            batch_idx = np.random.choice(idx, self.conf['batch_size'], replace=False)
+            batch_idx = np.random.choice(idx, self.conf["modelParams"]["modelData"]['batch_size'], replace=False)
             # print(batch_idx)
 
             x = self.data_x[batch_idx]
@@ -125,7 +125,7 @@ class Client(object):
             encrypted_grad = batch_encrypted_grad.sum(axis=1) / y.shape[0]
 
             for j in range(len(self.local_model.encrypt_weights)):
-                self.local_model.encrypt_weights[j] -= self.conf["lr"] * encrypted_grad[j]
+                self.local_model.encrypt_weights[j] -= self.conf["modelParams"]["modelData"]["lr"] * encrypted_grad[j]
 
         weight_accumulators = []
         # print(models.decrypt_vector(Server.private_key, weights))
@@ -166,90 +166,89 @@ def start_server(config):
     server = Server(config, eval_datasets)
     test_acc = []
     train_size = train_datasets[0].shape[0]
-    per_client_size = int(train_size / config["no_models"])
+    per_client_size = int(train_size / config["modelParams"]["modelData"]["no_models"])
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((config["server_id"], config["port"]))
-    server_socket.listen(2)
+    server_socket.bind((config["baseConfig"]["modelControlUrl"], 12345))
+    server_socket.listen(len(config["baseConfig"]["modelCalUrlList"]))
     print("服务器启动，等待连接...")
 
     config['role'] = 'client'
     threads = []
-    # 测试
-    for key in config:
+
+    for entry in config['baseConfig']['modelCalUrlList']:
+        client_url = ""
         # 实际用这个
-        """if "client" in key:
-            client_url = f'http://{config[key]}:5000/process_data'
-            thread = threading.Thread(target=send_request, args=(client_url, data))
-            threads.append(thread)
-            thread.start()"""
-        if "client1" in key:
-            client_url = f'http://{config[key]}:5001/process_data'
-            thread = threading.Thread(target=send_request, args=(client_url, config))
-            threads.append(thread)
-            thread.start()
-        if "client2" in key:
-            client_url = f'http://{config[key]}:5003/process_data'
-            thread = threading.Thread(target=send_request, args=(client_url, config))
-            threads.append(thread)
-            thread.start()
+        """user_id = entry['userId']
+        url = entry['url']
+        is_initiator = entry['isInitiator']
+        # 角色是否区分待定
+        if is_initiator:
+            client_url = f'http://{url}:5000/process_data'
+        else:
+            client_url = f'http://{url}:5000/process_data'
 
-    client1, addr1 = server_socket.accept()
-    print(f'客户端{addr1}已连接')
-    client2, addr2 = server_socket.accept()
-    print(f'客户端{addr2}已连接')
+        thread = threading.Thread(target=send_request, args=(client_url, config))
+        threads.append(thread)
+        thread.start()"""
+        # 测试
+        user_id = int(entry['userId'])
 
-    client1_instance = Client(config, Server.public_key, server.global_model.encrypt_weights,
-                              train_datasets[0][0 * per_client_size: (0 + 1) * per_client_size],
-                              train_datasets[1][0 * per_client_size: (0 + 1) * per_client_size], 1)
-    client2_instance = Client(config, Server.public_key, server.global_model.encrypt_weights,
-                              train_datasets[0][1 * per_client_size: (1 + 1) * per_client_size],
-                              train_datasets[1][1 * per_client_size: (1 + 1) * per_client_size], 2)
+        url = entry['url']
+        is_initiator = entry['isInitiator']
 
-    instances = {config["client1_id"]: client1_instance, config["client2_id"]: client2_instance}
-    send_data(client1, pickle.dumps(instances))
-    send_data(client2, pickle.dumps(instances))
+        if user_id == 1:
+            client_url = f'http://{url}:5001/process_data'
+        elif user_id == 129:
+            client_url = f'http://{url}:5003/process_data'
 
-    for e in range(config["global_epochs"]):
+        thread = threading.Thread(target=send_request, args=(client_url, config))
+        threads.append(thread)
+        thread.start()
+
+    clients = []
+    instances = {}
+    # 动态接受客户端连接并创建实例
+    for i, model_url in enumerate(config["baseConfig"]["modelCalUrlList"]):
+        client_socket, addr = server_socket.accept()
+        print(f'客户端{addr}已连接')
+        client_instance = Client(config, Server.public_key, server.global_model.encrypt_weights,
+                                 train_datasets[0][i * per_client_size: (i + 1) * per_client_size],
+                                 train_datasets[1][i * per_client_size: (i + 1) * per_client_size], i+1)
+        clients.append(client_socket)
+        instances[model_url["url"]] = client_instance
+
+    # 发送实例到客户端
+    for client_socket in clients:
+        send_data(client_socket, pickle.dumps(instances))
+
+    for e in range(config["modelParams"]["modelData"]["global_epochs"]):
         print(f"Global Epoch {e + 1}")
         # 重新加密全局模型权重
         server.global_model.encrypt_weights = models.encrypt_vector(Server.public_key,
                                                                     models.decrypt_vector(Server.private_key,
                                                                                           server.global_model.encrypt_weights))
-        weight_accumulator = {config["client1_id"]: [Server.public_key.encrypt(0.0)] * (config["feature_num"] + 1),
-                              config["client2_id"]: [Server.public_key.encrypt(0.0)] * (config["feature_num"] + 1)}
+        weight_accumulator = [Server.public_key.encrypt(0.0)] * (config["modelParams"]["modelData"]["feature_num"] + 1)
+        data_to_send = {model_url["url"]: server.global_model.encrypt_weights for model_url in config["baseConfig"]["modelCalUrlList"]}
+        for client_socket in clients:
+            send_data(client_socket, pickle.dumps(data_to_send))
 
-        data_to_send = {config["client1_id"]: server.global_model.encrypt_weights,
-                        config["client2_id"]: server.global_model.encrypt_weights}
-        send_data(client1, pickle.dumps(data_to_send))
-        send_data(client2, pickle.dumps(data_to_send))
+        for client_socket in clients:
+            updated_data = pickle.loads(recv_data(client_socket))
+            for model_url in config["baseConfig"]["modelCalUrlList"]:
+                if model_url["url"] in updated_data:
+                    diff = updated_data[model_url["url"]]
+                    for i in range(len(weight_accumulator)):
+                        weight_accumulator[i] += diff[i]
 
-        updated_data1 = pickle.loads(recv_data(client1))
-        updated_data2 = pickle.loads(recv_data(client2))
-
-        if config["client1_id"] in updated_data1:
-            diff = updated_data1[config["client1_id"]]
-            for i in range(len(weight_accumulator[config["client1_id"]])):
-                weight_accumulator[config["client1_id"]][i] += diff[i]
-
-        if config["client2_id"] in updated_data2:
-            diff = updated_data2[config["client2_id"]]
-            for i in range(len(weight_accumulator[config["client2_id"]])):
-                weight_accumulator[config["client2_id"]][i] += diff[i]
-
-        aggregated_weights = [Server.public_key.encrypt(0.0)] * (config["feature_num"] + 1)
-        for client_id in weight_accumulator:
-            for i in range(len(aggregated_weights)):
-                aggregated_weights[i] += weight_accumulator[client_id][i]
-
-        server.model_aggregate(aggregated_weights)
+        server.model_aggregate(weight_accumulator)
         acc, global_model_weights = server.model_eval()
 
         test_acc.append(float(acc / 100))
-        json_data = {"global_epochs": config["global_epochs"], "Accuracy": test_acc,
+        json_data = {"global_epochs": config["modelParams"]["modelData"]["global_epochs"], "Accuracy": test_acc,
                      "model_parameter": global_model_weights}
 
-        if e == config["global_epochs"] - 1:
+        if e == config["modelParams"]["modelData"]["global_epochs"] - 1:
             filename = './results/results_HE.json'
             with open(filename, 'w') as file_obj:
                 json.dump(json_data, file_obj, indent=4)
@@ -259,8 +258,8 @@ def start_server(config):
 
         print(f"Global Epoch {e + 1}, acc: {acc}\n")
 
-    client1.close()
-    client2.close()
+    for client_socket in clients:
+        client_socket.close()
     server_socket.close()
 
     plt.figure()
@@ -281,12 +280,12 @@ def start_client(config):
 
     train_datasets, eval_datasets = read_dataset()
     train_size = train_datasets[0].shape[0]
-    per_client_size = int(train_size / config["no_models"])
+    per_client_size = int(train_size / config["modelParams"]["modelData"]["no_models"])
 
     # 创建socket对象
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # 连接到服务器
-    client_socket.connect((config["server_id"], config["port"]))
+    client_socket.connect((config["baseConfig"]["modelControlUrl"], 12345))
     print(f"{client1_id}已连接服务器")
     # 接收服务器发来的客户端实例（使用特殊方法接收大数据）
     # client_instance = client_socket.recv(10240)
--- homomorphic_encryption_serv/server/platform_test.py
+++ homomorphic_encryption_serv/server/platform_test.py
@@ -6,24 +6,85 @@ BASE_URL = "http://127.0.0.1:5002"  # 请根据实际情况修改
 def send_data_and_get_response():
     # 构建要发送的JSON数据
     data = {
-    "client1_id": "127.0.0.1",
-    "client2_id": "127.0.0.1",
-    "server_id": "127.0.0.1",
-    "port": 12345,
-    "no_models": 3,
-    "global_epochs": 3,
-    "local_epochs": 2,
-    "k": [
-        2,
-        3
-    ],
-    "batch_size": 32,
-    "lr": 0.15,
-    "momentum": 0.0001,
-    "lambda": 0.5,
-    "feature_num": 30,
-    "role": "server"
-}
+        "projectJobId": "1780926403611033645",
+        "role": "server",
+        "baseConfig": {
+            "modelControlUrl": "127.0.0.1",
+            "modelCalUrlList": [
+                {
+                    "userId": "129",
+                    "url": "127.0.0.1",
+                    "isInitiator": False
+                },
+                {
+                    "userId": "1",
+                    "url": "127.0.0.1",
+                    "isInitiator": True
+                }
+            ],
+            "platGwUrl": "http://localhost:48080"
+        },
+        "modelParams": {
+            "dataSet": {
+                "projectOrgans": [
+                    {
+                        "userId": "129",
+                        "userType": 2,
+                        "resource": [
+                            {
+                                "id": "1780926403611033602",
+                                "dataSetName": "必填测试",
+                                "dataSetDesc": "23",
+                                "dataSetFilePath": None,
+                                "dataSetFileName": None,
+                                "dataSetFileUrl": None,
+                                "visibility": 2,
+                                "visibilityUsers": "admin",
+                                "remark": None,
+                                "status": None,
+                                "creator": "129"
+                            }
+                        ]
+                    },
+                    {
+                        "userId": "1",
+                        "userType": 1,
+                        "resource": [
+                            {
+                                "id": "1781142905056477185",
+                                "dataSetName": "测试更新",
+                                "dataSetDesc": "2324",
+                                "dataSetFilePath": None,
+                                "dataSetFileName": None,
+                                "dataSetFileUrl": None,
+                                "visibility": 1,
+                                "visibilityUsers": "",
+                                "remark": None,
+                                "status": None,
+                                "creator": "1"
+                            }
+                        ]
+                    }
+                ]
+            },
+            "modelData": {
+                "modelId": "LR",
+                "securityProtocol": "同态加密",
+                "no_models": 3,
+                "global_epochs": 3,
+                "local_epochs": 2,
+                "k": [
+                    2,
+                    3
+                ],
+                "batch_size": 32,
+                "lr": 0.15,
+                "momentum": 0.0001,
+                "lambda": 0.5,
+                "feature_num": 30
+            }
+        }
+    }
     # 向 /process_data 端点发送POST请求，附带JSON数据
     response = requests.post(f"{BASE_URL}/process_data", json=data)
 
